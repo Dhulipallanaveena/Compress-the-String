227642c4eaf1cb34fef065d6382c30cf423617a4
Dhulipallanaveena <81727940+Dhulipallanaveena@users.noreply.github.com>
2021-10-31 22:14:01 +0530

    Create Compress the String.py

---
+++ Compress the String.py
@@ -0,0 +1,19 @@
+s=input()
+res=[]
+i=0
+while i<len(s):
+    count=1
+    j=i+1
+    while j<len(s):
+        if s[i]==s[j]:
+            count=count+1
+            j=j+1
+        else:
+            break
+    if count>1:
+        res.append(s[i]+str(count))
+    else:
+        res.append(s[i])
+    i=i+count
+for i in range(0,len(res)):
+    print(res[i],end="")
